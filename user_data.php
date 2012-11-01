a4fa16fd7b8b3d6f5982b499f95e830f267ae135
Jeff Tilson <jrtilson@gmail.com>
2012-11-01 15:54:45 +0000

    Added script to dump user metadata

---
+++ user_data.php
@@ -0,0 +1,27 @@
+<?php
+// Get user info
+require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+admin_gatekeeper();
+
+$user = get_user_by_username(get_input('u'));
+
+
+echo "<pre>";
+
+if (elgg_instanceof($user, 'user')) {
+	
+	$metadata = elgg_get_metadata(array(
+		'guid' => $user->guid,
+	));
+
+	echo "Dumping Metadata:\r\n\r\n";
+
+	foreach ($metadata as $md) {
+		echo $md->name . ": " . $md->value . "\r\n";
+	}
+
+} else {
+	echo "No user";
+}
+
+echo "</pre>";
